3624109fcc1af9cba547519a17dacf9879db03f6
JuanDGra <juangraja13@gmail.com>
2021-07-11 18:15:51 -0500

    Ahi vamos

--- Unidad 2/Trabajo 2/src/App.java
+++ Unidad 2/Trabajo 2/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int numero = 56;
+        perro pepe = new perro();
+        System.out.println(pepe.perseguirCola);
     }
 }
---
+++ Unidad 2/Trabajo 2/src/perro.java
@@ -0,0 +1,11 @@
+public class perro {
+    // Atributos
+     // public, private. protected
+    private boolean perseguirCola = true;
+    private String orinar;
+    private String nombre;
+    
+
+    // Constructor
+    // Métodos
+}
