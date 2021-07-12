3b2cdcf91f5706ff5453a2873fc22db653913bb3
JuanDGra <juangraja13@gmail.com>
2021-07-12 16:31:28 -0500

    nada importante

--- Unidad 2/Trabajo 2/src/App2.java
+++ Unidad 2/Trabajo 2/src/App2.java
@@ -4,9 +4,10 @@ public class App2 {
         Perro perro2 = new Perro("Trosky",3,"Sentado");        
         Perro perro3 = new Perro("Manchis",8,"Parado");        
         Perro perro4 = new Perro("Melquiades",4,"Sentado");        
-        //System.out.println(perro1.contadorPerros);
-        //System.out.println(perro2.contadorPerros);
-        //System.out.println(perro3.contadorPerros);
+        System.out.println(perro1.getNombre());
+        System.out.println(perro2.getNombre());
+        System.out.println(perro3.getNombre());
+        System.out.println(perro4.getNombre());
         System.out.println(Perro.FormaDeDientes);
         System.out.println(Perro.contadorPerros);
         Perro.oler();
